47ad5df223086669546de7f1426baf1c75c08404
Helen Lam <helen.f.lam100@gmail.com>
2014-12-15 19:17:44 -0800

    initial commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,19 @@
 class CashRegister
-  def total
-    0
+
+  def initialize
+    @total = 0
   end
+
+  def total
+    @total
+  end
+
+  def purchase(price)
+    @total += price.to_f
+  end
+
+  def pay(amount_person_paying)
+    amount_person_paying.to_f - @total
+  end
+
 end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,13 +1,46 @@
 require 'cash_register'
 
 describe CashRegister do
+
   describe "#total" do
-    it 'starts at 0' do
-      cash_register = CashRegister.new
+    it 'starts at 0' do        #describe
+      cash_register = CashRegister.new  #instantiating a new class
 
-      total = cash_register.total
+      total = cash_register.total   #do the thing you're testing
 
-      expect(total).to eq(0)
+      expect(cash_register.total).to eq(0)
     end
   end
+
+  describe "#purchase" do
+    it 'takes a floating number and adds to total' do
+      cash_register = CashRegister.new
+
+      cash_register.purchase(15.75)
+
+      expect(cash_register.total).to eq(15.75)
+    end
+  end
+
+
+
+  describe "#pay" do
+    it 'takes one floating number for how much is paid. It should return how much change is given' do
+      cash_register = CashRegister.new
+
+      cash_register.purchase(15.75)
+      change = cash_register.pay(16.00)
+
+      expect(change).to eq(0.25)
+    end
+
+  end
+
 end
+
+#look at thoughtbot for rspec tests
+
+#   There is a purchase method that takes a floating number and adds that to the total.
+# There is a total method that returns how much is owed.
+# There is a pay method that takes one floating number for how much is paid. It should return how much change is given.
+# When the Cash Register is initialized is should begin with a starting balance of 0.
